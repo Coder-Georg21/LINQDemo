0b18b685330e5daa7d81bb888af358a4bfccc509
georg.krautinger <k4KEo9q7>
2021-12-10 12:52:21 +0100

    Projektdateien hinzufügen.

---
+++ City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+
+        public IList<City> Factory()
+        {
+            IList<City> cityList = new List<City>();
+
+            cityList.Add(new City { Id =1, Name = "Wien", Population = 1900000 });
+            cityList.Add(new City { Id =2, Name = "Paris", Population = 2100000 });
+            cityList.Add(new City { Id =3, Name = "NewYork", Population = 8400000 });
+            return cityList;
+        }
+    }
+}
